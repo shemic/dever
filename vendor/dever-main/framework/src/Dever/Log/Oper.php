b2dbb87ab187a958754d470d0ce882dbad2616f3
dever <rabin@dever.cc>
2018-04-11 06:51:31 +0000

    edit

--- vendor/dever-main/framework/src/Dever/Log/Oper.php
+++ vendor/dever-main/framework/src/Dever/Log/Oper.php
@@ -4,23 +4,24 @@ use Dever\Loader\Config;
 
 class Oper
 {
-    public static function add($msg)
+    # 1为debug 2为notice 3为info
+    public static function add($msg, $type = 1)
     {
         $log = '';
         if (is_array($msg)) {
             foreach($msg as $k => $v)
             {
-                $log .= " ^".ucfirst($k).":" . self::filter($v);
+                $log .= "&".$k."=" . self::filter($v);
             }
         } else {
             $log = $msg;
         }
 
-        self::push($log);
+        self::push($log, $type);
         
     }
 
-    private static function push($log)
+    private static function push($log, $type = 1)
     {
         if (is_array(Config::get('debug')->log) && isset(Config::get('debug')->log['type']) && Config::get('debug')->log['type'] == 'udp') {
             $method = Config::get('debug')->log['type'];
@@ -29,8 +30,19 @@ class Oper
             $class->client(Config::get('debug')->log['host'], Config::get('debug')->log['port']);
             $class->push($log);
         } else {
-            openlog(DEVER_APP_NAME,LOG_PID,LOG_LOCAL3);
-            syslog(LOG_DEBUG,$log);
+            if ($type == 1) {
+                $type = LOG_DEBUG;
+                $name = LOG_LOCAL1;
+            } elseif ($type == 2) {
+                $type = LOG_NOTICE;
+                $name = LOG_LOCAL2;
+            } elseif ($type == 3) {
+                $type = LOG_INFO;
+                $name = LOG_LOCAL3;
+            }
+
+            openlog(DEVER_APP_NAME, LOG_PID, $name);
+            $state = syslog($type, $log);
             closelog();
         }
     }
