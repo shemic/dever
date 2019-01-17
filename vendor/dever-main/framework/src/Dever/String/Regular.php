bb8a273709c269e560c44e39d8dd93bac321b51c
dever <dm@dever.cc>
2019-01-17 03:11:31 +0000

    edit

--- vendor/dever-main/framework/src/Dever/String/Regular.php
+++ vendor/dever-main/framework/src/Dever/String/Regular.php
@@ -2,19 +2,28 @@
 
 class Regular
 {
-	public static function rule($method, $fix = '/')
+	public static function rule($method, $fix = '/', $rule = '')
     {
         $method = 'rule_' . $method;
-        return $fix . self::$method() . $fix;
+        return $fix[0] . self::$method($rule) . $fix;
     }
 
-    public static function rule_mobile()
+    # 手机号
+    public static function rule_mobile($rule)
     {
         return '^(1([3456789][0-9]))\d{8}$';
     }
 
-    public static function rule_email()
+    # 邮箱
+    public static function rule_email($rule)
     {
         return '^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$';
+    }
+
+    # 中文
+    public static function rule_zh($rule)
+    {
+        $rule = $rule ? $rule : 8;
+        return '^([\x{4e00}-\x{9fa5}]){'.$rule.'}$';
     }
 }
