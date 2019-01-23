676943fae737c88a97829d4bf2720948a00f54c1
dever <dm@dever.cc>
2019-01-23 09:13:14 +0000

    edit

--- vendor/dever-main/framework/src/Dever/Cache/Handle.php
+++ vendor/dever-main/framework/src/Dever/Cache/Handle.php
@@ -132,10 +132,14 @@ class Handle
         $data = $this->store->get($key);
         $data = json_decode(base64_decode($data), true);
         $this->log('get', $key, $data, $this->expire($key));
+
+        /*
         if ($page = $this->store->get('page_' . $key)) {
             $page = json_decode(base64_decode($page), true);
             Dever::$global['page'] = $page;
         }
+        */
+
         return $data;
     }
 
@@ -153,9 +157,12 @@ class Handle
         $this->init($key, $expire);
         $this->log('set', $key, $value, $expire);
         $value = base64_encode(json_encode($value));
+
+        /*
         if (isset(Dever::$global['page']) && Dever::$global['page']) {
             $this->store->set('page_' . $key, base64_encode(json_encode(Dever::$global['page'])), $expire);
         }
+        */
         
         return $this->store->set($key, $value, $expire);
     }
