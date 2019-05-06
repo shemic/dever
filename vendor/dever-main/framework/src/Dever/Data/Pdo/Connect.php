9bc26cd8eba3f84ab61b8afcca42186255cd5c04
dever <dm@dever.cc>
2019-05-06 09:19:33 +0000

    edit

--- vendor/dever-main/framework/src/Dever/Data/Pdo/Connect.php
+++ vendor/dever-main/framework/src/Dever/Data/Pdo/Connect.php
@@ -63,7 +63,11 @@ class Connect
         $dsnList = $type . ':' . implode(';', $dsn);
 
         try {
-            $this->handle = new \PDO($dsnList, $config['username'], $config['password'], array(\PDO::ATTR_PERSISTENT => true));
+            $persistent = false;
+            if (isset($config['persistent'])) {
+                $persistent = $config['persistent'];
+            }
+            $this->handle = new \PDO($dsnList, $config['username'], $config['password'], array(\PDO::ATTR_PERSISTENT => $persistent));
             $this->handle->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             //$this->handle->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
             $this->handle->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_NATURAL);
